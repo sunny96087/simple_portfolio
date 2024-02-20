f628b5d9cae24854395de847fa579a9b2e7ac499
sunny96087 <sunny96087@yahoo.com.tw>
2024-02-20 15:25:15 +0800

    圖片抓取問題 測試

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,62 +1,105 @@
 <script setup>
+// 從 firebase 抓取檔案
+const projectPic1 = ref(
+  'https://firebasestorage.googleapis.com/v0/b/simpleportfolio-64b60.appspot.com/o/img%2Fproject-pic-1.png?alt=media&token=388b6fc4-a863-4d12-9e55-f81de4e8c489'
+)
+const viewPic1 = ref(
+  'https://firebasestorage.googleapis.com/v0/b/simpleportfolio-64b60.appspot.com/o/img%2Fview-pic-1.png?alt=media&token=13087de1-32cd-4aaf-b85e-033c69b68c6f'
+)
+
+const projectPic2 = ref(
+  'https://firebasestorage.googleapis.com/v0/b/simpleportfolio-64b60.appspot.com/o/img%2Fproject-pic-2.png?alt=media&token=a9f35dcc-8b4a-4424-ab54-a9d06d3d1cd1'
+)
+const viewPic2 = ref(
+  'https://firebasestorage.googleapis.com/v0/b/simpleportfolio-64b60.appspot.com/o/img%2Fview-pic-2.png?alt=media&token=36aa361a-36a0-4fb8-adbd-5336ff19fd8a'
+)
+
+const projectPic3 = ref(
+  'https://firebasestorage.googleapis.com/v0/b/simpleportfolio-64b60.appspot.com/o/img%2Fproject-pic-3.png?alt=media&token=be7338e8-883e-4ccc-b979-5bbe52df4e94'
+)
+const viewPic3 = ref(
+  'https://firebasestorage.googleapis.com/v0/b/simpleportfolio-64b60.appspot.com/o/img%2Fview-pic-3.png?alt=media&token=8d269970-600f-4f75-a6fb-759bc0e9cd7a'
+)
+
+const projectPic4 = ref(
+  'https://firebasestorage.googleapis.com/v0/b/simpleportfolio-64b60.appspot.com/o/img%2Fproject-pic-4.png?alt=media&token=d2701bfb-cedb-45e7-b37d-d312ba9763b1'
+)
+const viewPic4 = ref(
+  'https://firebasestorage.googleapis.com/v0/b/simpleportfolio-64b60.appspot.com/o/img%2Fview-pic-4.png?alt=media&token=66d2659d-b181-42c0-ac88-9933a6e05da2'
+)
+
 const projects = ref([
   {
-    pic: 'src/assets/img/project-pic-1.png',
+    pic: projectPic1,
     type: 'WEB',
     name: '軌道漫遊列車之旅',
     description:
       '這是一個團隊合作開發的網站項目，<br>旨在提供使用者一個互動式的平台，探索和計劃他們的小火車旅行。<br>在這個項目中，我參與了從概念發想到最終產品的全過程。<br>我負責網站的介面設計和前後端開發，使用 Vue.js 和 SCSS 來創建響應式的用戶界面，並透過 JavaScript 增強互動性。<br>後端方面，我使用 PHP 和 MySQL 進行資料庫的規劃和建置，並利用 Ajax 和 Axios 技術實現前後端的資料串接。<br>這個項目不僅鍛鍊了我的全端開發能力，也讓我學習到團隊合作如何溝通及解決問題。',
     github: 'https://github.com/sunny96087/project',
     demo: 'https://sunny96087.github.io/project/',
-    viewPic: 'src/assets/img/view-pic-1.png',
+    viewPic: viewPic1,
     marquee:
       '軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ 軌道漫遊列車之旅 ・ '
   },
   {
-    pic: 'src/assets/img/project-pic-2.png',
+    pic: projectPic2,
     type: 'WEB',
     name: '餓魚漢堡',
     description:
       '這是一個自行構想和設計的漢堡餐廳介紹靜態網站，提供菜單、門市查找、最新消息及食品保證書查看，使用HTML、CSS、JavaScript和jQuery技術開發。',
     github: 'https://github.com/vicky5645/chd102-g1/tree/dev',
     demo: 'https://tibamef2e.com/chd102/g1/',
-    viewPic: 'src/assets/img/view-pic-2.png',
+    viewPic: viewPic2,
     marquee:
       '餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ 餓魚漢堡 ・ '
   },
   {
-    pic: 'src/assets/img/project-pic-3.png',
+    pic: projectPic3,
     type: 'WEB',
     name: '虛擬貓咪總統大選開票網站',
     description:
       '這是一個團隊合作開發的虛擬貓咪總統大選開票網站，為六角學院2023年The F2E 5th修煉精神時光屋比賽的作品。<br>採用圖像化呈現得票數據，讓使用者輕易掌握選情。<br>我負責首頁、候選人介紹頁及Navbar的開發，使用技術包括JavaScript ES6、Vue3 + Vite、Scss、RWD 和 Git 。<br>遇到的挑戰包括首頁不規則排版和候選人頁面的資料抓取。<br>此外，我還負責製作桌機及手機版的prototype。',
     github: 'https://github.com/edwardtsai54398/F2Evotefortw2024',
     demo: 'https://edwardtsai54398.github.io/F2Evotefortw2024/#/',
-    viewPic: 'src/assets/img/view-pic-3.png',
+    viewPic: viewPic3,
     marquee:
       '虛擬貓咪總統大選開票網站 ・ 虛擬貓咪總統大選開票網站 ・ 虛擬貓咪總統大選開票網站 ・ 虛擬貓咪總統大選開票網站 ・ 虛擬貓咪總統大選開票網站 ・ 虛擬貓咪總統大選開票網站 ・ 虛擬貓咪總統大選開票網站 ・ 虛擬貓咪總統大選開票網站 ・ 虛擬貓咪總統大選開票網站 ・ 虛擬貓咪總統大選開票網站 ・ 虛擬貓咪總統大選開票網站 ・ 虛擬貓咪總統大選開票網站 ・ 虛擬貓咪總統大選開票網站 ・ 虛擬貓咪總統大選開票網站 ・ 虛擬貓咪總統大選開票網站 ・ 虛擬貓咪總統大選開票網站 ・ 虛擬貓咪總統大選開票網站 ・ 虛擬貓咪總統大選開票網站 ・ 虛擬貓咪總統大選開票網站 ・ 虛擬貓咪總統大選開票網站 ・ 虛擬貓咪總統大選開票網站 ・ '
   },
   {
-    pic: 'src/assets/img/project-pic-4.png',
+    pic: projectPic4,
     type: 'WEB',
     name: '拼拼 - 募資網站',
     description:
       '這是一個靜態募資網站，包含專案介紹、常見問答、目前進度、留言、方案選擇和贊助專案等區塊。<br>網站主要使用HTML、CSS、Bootstrap、JavaScript 技術開發。',
     github: 'https://github.com/sunny96087/BT5_pinpin',
     demo: 'https://sunny96087.github.io/BT5_pinpin/#',
-    viewPic: 'src/assets/img/view-pic-4.png',
+    viewPic: viewPic4,
     marquee:
       '拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ 拼拼 - 募資網站 ・ '
   }
 ])
 
-import { ref } from 'vue'
+import { ref, onMounted, onUnmounted } from 'vue'
 
 const selectedProject = ref(null)
 
 const openModal = (project) => {
   selectedProject.value = project
 }
+
+const closeOnEsc = (event) => {
+  if (event.key === 'Escape') {
+    selectedProject.value = null;
+  }
+};
+
+onMounted(() => {
+  window.addEventListener('keydown', closeOnEsc);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('keydown', closeOnEsc);
+});
 </script>
 
 <template>
--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ViteImages from 'vite-plugin-vue-images'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue(), ViteImages()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
