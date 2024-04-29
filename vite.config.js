f83a78527fc34f1dd32a46dba3cab8188267e351
sunny96087 <sunny96087@yahoo.com.tw>
2024-04-29 22:06:33 +0800

    放到 google search, 增加 sitemap

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import ViteImages from 'vite-plugin-vue-images'
+import sitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), ViteImages()],
+  plugins: [vue(), ViteImages(),
+    sitemap({
+      // 這裡是套件的配置選項
+      // 例如，你可以設定 sitemap 的基本路徑
+      baseUrl: 'https://yourdomain.com',
+      // 或者，你可以指定生成 sitemap 的路徑
+      outputPath: 'dist/sitemap.xml',
+      // 更多配置選項，請參考套件文檔
+    }),],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
