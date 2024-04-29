61fc2d603c7a762a1660a87d4c2a5ea3b0901491
sunny96087 <sunny96087@yahoo.com.tw>
2024-04-29 22:20:45 +0800

    更新 sitemap

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import ViteImages from 'vite-plugin-vue-images'
-import sitemap from 'vite-plugin-sitemap';
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), ViteImages(),
+  plugins: [
+    vue(),
+    ViteImages(),
     sitemap({
-      // 這裡是套件的配置選項
-      // 例如，你可以設定 sitemap 的基本路徑
-      baseUrl: 'https://yourdomain.com',
-      // 或者，你可以指定生成 sitemap 的路徑
-      outputPath: 'dist/sitemap.xml',
-      // 更多配置選項，請參考套件文檔
-    }),],
+      // 設定 sitemap 的基本路徑
+      baseUrl: 'https://simpleportfolio-64b60.web.app/',
+      // 指定生成 sitemap 的路徑
+      outputPath: 'dist/sitemap.xml'
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
